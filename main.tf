78cf56b1f2d69a4a90d81083b6a74eb1e62b8c3f
implodingduck <zsek320@gmail.com>
2021-07-13 16:21:06 -0500

    working on gettingg a function deployed

---
+++ main.tf
@@ -0,0 +1,50 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.62.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "=3.1.0"
+    }
+  }
+  backend "azurerm" {
+
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+}
+
+locals {
+  func_name = "sctf${random_string.unique.result}"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-logicapp-sql-con-test"
+  location = var.location
+}
+
+resource "random_string" "unique" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+module "function" {
+    source = "github.com/implodingduck/tfmodules//functionapp"
+    func_name = local.func_name
+    resource_group_name = azurerm_resource_group.rg.name
+    resource_group_location = azurerm_resource_group.rg.location
+    working_dir = "SqlConTestFunc"
+    app_settings = {
+      "FUNCTIONS_WORKER_RUNTIME" = "python"
+      
+    }
+
+}
