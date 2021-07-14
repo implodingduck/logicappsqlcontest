15774f20ed2f078fec716c9a0fd22ec61878eba0
implodingduck <zsek320@gmail.com>
2021-07-13 20:35:00 -0500

    adding a keyvault

--- main.tf
+++ main.tf
@@ -25,6 +25,8 @@ locals {
   func_name = "sctf${random_string.unique.result}"
 }
 
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "rg" {
   name     = "rg-logicapp-sql-con-test"
   location = var.location
@@ -46,5 +48,45 @@ module "function" {
       "FUNCTIONS_WORKER_RUNTIME" = "python"
       
     }
+
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                       = "logappsqlcontest-kv"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled = false
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "create",
+      "get",
+      "purge",
+      "recover",
+      "delete"
+    ]
+
+    secret_permissions = [
+      "set",
+      "purge",
+      "get",
+      "list"
+    ]
+
+    certificate_permissions = [
+      "purge"
+    ]
+
+    storage_permissions = [
+      "purge"
+    ]
+  }
+
 
 }
